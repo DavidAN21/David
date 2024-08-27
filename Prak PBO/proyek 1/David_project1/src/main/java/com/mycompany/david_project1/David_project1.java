7f5f0444187afcc28f4c87d1eedc9646ff3f728f
David Andrey Nugroho <davidandrea0429@gmail.com>
2024-08-27 20:58:41 +0700

    david

---
+++ Prak PBO/proyek 1/David_project1/src/main/java/com/mycompany/david_project1/David_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.david_project1;
+
+/**
+ *
+ * @author David Andrey
+ */
+public class David_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama  : David Andrey Nugroho");
+        System.out.println("NPM   : 2340506053");
+        System.out.println("Alamat: Pasaranom, Grabag, Purworejo");
+        System.out.println("No. Hp: 083844577137");
+    }
+}
